0c86ecd05f0ad3615014f1c1e234a7a2e465e85b
Agu C. Christopher <aguchris740@gmail.com>
2023-06-05 06:01:12 -0700

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Phynx Cart</Link>
+        <Link href="/">Phanox Cart</Link>
       </p>
 
       <button
